c71d43a9b6d4f5c0f0d0b06be3bc467bd42b5073
Eros Maurilio <eros.maurilio@gmail.com>
2021-10-07 16:58:02 -0300

    Initial Commit

---
+++ CrashChallenge4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CrashChallenge4
+//
+//  Created by Eros Maurilio on 07/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CrashChallenge4/CrashChallenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  CrashChallenge4App.swift
+//  CrashChallenge4
+//
+//  Created by Eros Maurilio on 07/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CrashChallenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
